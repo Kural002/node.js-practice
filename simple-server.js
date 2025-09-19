9326f7844951a740f287758a3bab022e538d425f
Kural002 <kuralarasu2004@gmail.com>
2025-09-19 12:32:33 +0530

    video streaming

--- simple-server.js
+++ simple-server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     if (req.url == '/') {
@@ -7,6 +8,11 @@ const server = http.createServer((req, res) => {
 } else if (req.url == '/about') {
     res.writeHead(200 , {'content-type' : 'text/plain'});
     res.end("About Us");
+} 
+else if (req.url == '/file') {
+    const readStream = fs.createReadStream("./sample.mp4");
+    res.writeHead(200 , {'content-type' : 'video/mp4'});
+    readStream.pipe(res);
 } 
 else {
     res.writeHead(404 , {'content-type' : 'text/plain'});
